562b576182f9af978cd9e72c826184680eae6a8b
surajvbhattathiri <surajv.bhattathiri@nielsen.com>
2021-02-23 10:09:42 +0530

    fv

---
+++ bom-dom/bom.js
@@ -0,0 +1,34 @@
+// window.parseInt('12345');
+// alert(window.navigator.userAgent);
+
+// if (typeof window.addEventListener === 'function') {
+//   alert('Event support is there');
+// } else {
+//   alert('not supported');
+// }
+
+// window.location.href = 'http://www.espncricinfo.com';
+
+// history
+
+// alert(window.history.length)
+// history.forward()
+// history.back()
+// history.go(-3)
+
+// var win = window.open(
+//   'https://www.google.com',
+//   'MyWin',
+//   'width=200, height=300,resizable=no',
+// );
+
+// if (document.documentElement.hasChildNodes) {
+//   alert(document.documentElement.childNodes.length);
+//   alert(document.documentElement.childNodes[0]);
+// }
+
+// alert(document.getElementsByTagName('p').length);
+
+document.write('Hello :');
+
+// Event Capturing and Event Bubbling - learn
